cf6a9b1c99db3ffd59f9efd0a0ae6423d50307e7
ahmedabougabal <ahmedabougabal@live.com>
2024-08-02 22:52:41 +0300

    feat: added missing leetcode problem links, solutions, verified by Eng/Islam Kortam :D

---
+++ group_anagrams.cpp
@@ -0,0 +1,38 @@
+/*
+link : https://leetcode.com/problems/group-anagrams/
+*/
+
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+#include <string>
+using namespace std;
+//! if 2 words' sorting is the same, then they are anagrams :)
+class Solution
+{
+public:
+  vector<vector<string>> groupAnagrams(vector<string> &strs)
+  {
+    unordered_map<string, vector<string>> anagrams;
+    for (auto str : strs)
+    {
+      string sorted = str;
+      sort(sorted.begin(), sorted.end());
+      anagrams[sorted].push_back(str);
+    }
+    vector<vector<string>> result;
+    for (auto pair : anagrams)
+    {
+      result.push_back(pair.second);
+    }
+    return result;
+  }
+};
+
+/*
+time complexity :
+O(n * k log k)
+
+N is the number of strings, And k is the avg length of the string
+*/
