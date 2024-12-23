57eb853ad8bf876d58b65d28fffb229380dbb97e
ahmedabougabal <ahmedabougabal@live.com>
2024-12-24 01:16:06 +0200

    feat: adds execution results

--- closures/test.js
+++ closures/test.js
@@ -3,17 +3,18 @@
  * What is the expected output of the following code? What feature of javascript was used here?
  */
 
+// this is a closure 
 function getIncrementer(){
-  let cnt = 0;
-  return () => {
-    cnt++;
+  let cnt = 0; // private variable
+  return () => { // returns an inner function
+    cnt++; // accessing private variable (cnt is enclosed in the closure's scope)
     console.log(cnt);
   }
 }
 
 const increment = getIncrementer();
 
-increment();
-increment();
-increment();
+increment(); // 1
+increment(); // 2
+increment(); // 3
 
